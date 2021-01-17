1bec4ee0bdefc7b950f4f2d14e8593e347ba0b09
Ivan Torres <ivantorresmia27@gmail.com>
2021-01-17 14:08:58 -0800

    working on models and exporting for use with api

---
+++ models/books.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: false },
+  author: { type: Array, required: false },
+  description: {type: String, required: false},
+  date: { type: Date, default: Date.now },
+  image: {type: String, required: false},
+  link: {type: String, required: false}
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
