86ee298d5030c99451f287ff1996fb272e6fac28
Ivan Torres <ivantorresmia27@gmail.com>
2021-01-17 14:16:44 -0800

    controllers from creating and removing

--- controllers/controller.js
+++ controllers/controller.js
@@ -13,5 +13,19 @@ module.exports = {
         .findById(req.params.id)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
-    }
+    }, 
+    create: function(req, res) {
+        db.book
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {r
+        //   console.log("request",req);
+        db.Book
+          .findById({ _id: req.params.id })
+          .then(dbModel => dbModel.remove())
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      }
 }
