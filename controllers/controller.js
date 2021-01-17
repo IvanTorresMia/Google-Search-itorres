970ec59c665e4d4e97075a0de0eae366eb8fa318
Ivan Torres <ivantorresmia27@gmail.com>
2021-01-17 14:14:51 -0800

    build controller for findeing by id

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,5 +7,11 @@ module.exports = {
         .find(req.query)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) {
+        db.book
+        .findById(req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     }
 }
