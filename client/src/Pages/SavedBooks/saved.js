8d0b564208fb37515774f9e44201eadd169e909c
Ivan Torres <ivantorresmia27@gmail.com>
2021-01-20 15:29:34 -0800

    rendering saved books

--- client/src/Pages/SavedBooks/saved.js
+++ client/src/Pages/SavedBooks/saved.js
@@ -4,10 +4,18 @@ import API from '../../Utils/API'
 
 function Saved() {
 
+    const [books, setBooks] = useState([])
+
+
+    useEffect(() => {
+API.getApiBooks().then(res => {
+    setBooks(res.data)
+})
+    }, [books])
 
   return (
       <div>
-
+          <savedBooks books={books} deleteBooks={deleteBooks} />
       </div>
   )
 }
